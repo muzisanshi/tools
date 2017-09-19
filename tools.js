1a5fe0dac3c49f1d9f58858c5cd94606dc90c5ba
muzisanshi <1052048489@qq.com>
2017-09-19 13:39:21 +0800

    gggg
    gggg

--- tools.js
+++ tools.js
@@ -2,28 +2,26 @@
  * 一个常用工具函数的库。
  */
 
- ;function(){
+ ;(function(){
  	var tools = function(){
- 		return {
- 			// 获取指定cookie
- 			getCookie:function(name){
-		        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
-		        if(arr=document.cookie.match(reg)){
-		            return unescape(arr[2]);
-		        }else{
-		            return null;
-		        }
- 			},
- 			// 删除指定cookie
- 			delCookie:function(name){
-		        var exp = new Date();
-		        exp.setTime(exp.getTime() - 1000);
-		        var cval=getCookie(name);
-		        if(cval!=null){
-		            document.cookie= name + "="+cval+";expires="+exp.toGMTString()+";path=/";
-		        }
-		    }
- 		};
+		// 获取指定cookie
+		this.getCookie = function(name){
+	        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+	        if(arr=document.cookie.match(reg)){
+	            return unescape(arr[2]);
+	        }else{
+	            return null;
+	        }
+		};
+		// 删除指定cookie
+		this.delCookie = function(name){
+	        var exp = new Date();
+	        exp.setTime(exp.getTime() - 1000);
+	        var cval=getCookie(name);
+	        if(cval!=null){
+	            document.cookie= name + "="+cval+";expires="+exp.toGMTString()+";path=/";
+	        }
+	    }
  	};
     window.tools = tools;
- };
+ })();
